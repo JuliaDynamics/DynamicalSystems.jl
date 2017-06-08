8123af7eb827b63f040a479698472a618b606781
Datseris <datseris.george@gmail.com>
2017-06-08 19:37:53 +0200

    even more typos

--- test/discrete_lyapunov.jl
+++ test/discrete_lyapunov.jl
@@ -2,28 +2,28 @@
   @testset "Towel Map" begin
     @testset "λspectrum" begin
       ds = DynamicalSystems.Systems.towel()
-      λ1 = λspectrum(ds, Int(1e7))
+      λ1 = λspectrum(ds, 1e6)
       @test 0.42 < λ1[1] < 0.44
       @test 0.36 < λ1[2] < 0.38
-      @test -2.9 < λ1[3] < -3.1
+      @test -3.4 < λ1[3] < -3.2
     end
 
     @testset "λspectrum ForwardDiff" begin
       ds = DynamicalSystems.Systems.towel()
       ds = DiscreteDS(ds.state, ds.eom)
-      λ1 = λspectrum(ds, Int(1e7))
+      λ1 = λspectrum(ds, 1e6)
       @test 0.42 < λ1[1] < 0.44
       @test 0.36 < λ1[2] < 0.38
-      @test -2.9 < λ1[3] < -3.1
+      @test -3.4 < λ1[3] < -3.1
     end
 
     # test lmax
-    @testset "λmax" begin
-      #aasd
-    end
-
-    @testset "λspectrum benchmarks" begin
-      #asdf
-    end
+    # @testset "λmax" begin
+    #   #aasd
+    # end
+    #
+    # @testset "λspectrum benchmarks" begin
+    #   #asdf
+    # end
   end
 end
