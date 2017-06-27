83224277d7415b9dfad0569ca1488825dd2d91ff
George Datseris <datseris.george@gmail.com>
2017-06-27 15:29:54 +0200

    problems with lyapunov logistic

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,7 @@
+ti = time()
 include("discrete_systems.jl")
 include("discrete_lyapunov.jl")
 include("continuous_systems.jl")
+ti = time() - ti
+println("Test took total time of:")
+println(round(ti, 3), " seconds or ", round(ti/60, 3), " minutes")
