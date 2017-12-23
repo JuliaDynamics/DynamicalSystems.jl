c299c857d9edf2db239277d9a4be93b67a9b77ea
Datseris <datseris.george@gmail.com>
2017-12-23 14:37:41 +0100

    use conda

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,1 @@
 using DynamicalSystems
-
-# Basic tests if modules were loaded properly
-ds = Systems.towel()
-traj = trajectory(ds, 1000)
-R = Reconstruction(traj[:, 1], 2, 2)
-a = lyapunov(ds, 100)
