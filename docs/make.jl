31573693ca88c592149ced0b2f61a1c3cc92a5ca
George Datseris <datseris.george@gmail.com>
2018-03-07 11:21:42 +0100

    try pinning tornado

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, DynamicalSystems
 makedocs(modules=[DynamicalSystems], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs",
+    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
     "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
     julia  = "0.6",
