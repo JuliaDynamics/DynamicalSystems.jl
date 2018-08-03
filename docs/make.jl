575574fd67b3c525f003edcc367bebbd934053a6
George Datseris <datseris.george@gmail.com>
2018-08-03 12:49:30 +0200

    use nightl;y to build

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,6 @@ deploydocs(
     deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
     "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
-    julia  = "0.7",
+    julia  = "nightly",
     osname = "linux"
 )
