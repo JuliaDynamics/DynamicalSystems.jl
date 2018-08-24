3cffb57c9a024652c5e19c10ec28c37810354b57
George Datseris <datseris.george@gmail.com>
2018-08-24 17:04:00 +0300

    pin docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,16 @@
 using Documenter, DynamicalSystems, PyPlot
-
 PyPlot.ioff()
+
+# Add TimeseriesPrediction for documentation:
+using Pkg
+pkg"add TimeseriesPrediction#0.7-inplacerec"
+using TimeseriesPrediction
 
 makedocs(modules=[DynamicalSystems], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
-    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
+    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
+    "python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
     julia  = "nightly",
     osname = "linux"
