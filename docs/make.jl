6e377957f406d0f077eb9e3039d6f43c38f64e44
George Datseris <datseris.george@gmail.com>
2018-08-25 12:45:01 +0300

    build on 0.7

--- docs/make.jl
+++ docs/make.jl
@@ -12,6 +12,6 @@ deploydocs(
     deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
     "python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
-    julia  = "nightly",
+    julia  = "0.7",
     osname = "linux"
 )
