8282ca2c027c9f45b9601b86e20309b45a21c2f1
George Datseris <datseris.george@gmail.com>
2018-09-24 09:50:55 +0200

    finish s[patiotemporal examples

--- docs/src/tsprediction/stexamples.jl
+++ docs/src/tsprediction/stexamples.jl
@@ -1,1 +1,19 @@
 # # Spatio-Temporal Prediction Examples
+# In this page we are simply running files from the
+# `examples` folder of the `TimeseriesPrediction` package.
+
+# This is how you can (programmatically) find this folder:
+using TimeseriesPrediction
+exdir = dirname(dirname(pathof(TimeseriesPrediction)))*"/examples"
+
+# ## Temporal Prediction: Kuramoto-Sivashinsky
+
+include("1Dfield_temporalprediction.jl")
+
+# ## Cross Prediction: Barkley Model
+
+include("2Dfield_crossprediction.jl")
+
+# ## Temporal Prediction: Periodic Nonlinear Barkley Model
+
+include("2Dfield_temporalprediction.jl")
