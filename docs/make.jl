da66ad8226bc9b37f7e9283063eed52bd90a4c4f
George Datseris <datseris.george@gmail.com>
2018-09-24 11:34:11 +0200

    new examples page for ST

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,37 @@
-using Documenter, DynamicalSystems, PyPlot
+using Documenter, DynamicalSystems, PyPlot, TimeseriesPrediction
 PyPlot.ioff()
+cd(@__DIR__)
 
-using TimeseriesPrediction
+# Expand Spatio-temporal examples using Literate:
+using Literate
 
-makedocs(modules=[DynamicalSystems], doctest=false)
+function replace_includes(str)
 
-deploydocs(
-    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
-    "python-markdown-math", "pygments", "pymdown-extensions"),
-    repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
-    julia  = "1.0",
-    osname = "linux"
-)
+    included = ["1Dfield_temporalprediction.jl",
+    "2Dfield_crossprediction.jl", "2Dfield_temporalprediction.jl"]
+
+    path = dirname(dirname(pathof(TimeseriesPrediction)))*"/examples/"
+
+    for ex in included
+        content = read(path*ex, String)
+        str = replace(str, "include(\"$(ex)\")" => content)
+    end
+    return str
+end
+
+# Literate it:
+Literate.markdown("src/tsprediction/stexamples.jl", "src/tsprediction/";
+                  name = "stexamples", preprocess = replace_includes)
+
+
+makedocs(modules=[DynamicalSystems, TimeseriesPrediction], doctest=false)
+
+if !Sys.iswindows()
+    deploydocs(
+        deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
+        "python-markdown-math", "pygments", "pymdown-extensions"),
+        repo   = "github.com/JuliaDynamics/DynamicalSystems.jl.git",
+        julia  = "1.0",
+        osname = "linux"
+    )
+end
--- docs/src/tsprediction/stexamples.jl
+++ docs/src/tsprediction/stexamples.jl
@@ -7,6 +7,7 @@ using TimeseriesPrediction
 exdir = dirname(dirname(pathof(TimeseriesPrediction)))*"/examples"
 
 # ## Temporal Prediction: Kuramoto-Sivashinsky
+# *(this requires `FFTW` to be installed)*
 
 include("1Dfield_temporalprediction.jl")
 
