c00ee34bd222bc4c2a43d7642c3cb29a9e7d111f
George Datseris <datseris.george@gmail.com>
2018-12-15 17:15:35 +0100

    add notification for RQA

--- src/DynamicalSystems.jl
+++ src/DynamicalSystems.jl
@@ -14,19 +14,21 @@ using Reexport
 @reexport using ChaosTools
 @reexport using RecurrenceAnalysis
 
-display_update = false
-update_name = "update_v1.1.0"
+display_update = true
+update_name = "update_v1.2.0"
 
 if display_update
 if !isfile(joinpath(@__DIR__, update_name))
 printstyled(stdout,
 """
-\nUpdate message: DynamicalSystems v1.1
+\nUpdate message: DynamicalSystems v1.2
 
-Delay coordinates embedding & Dataset functionality moved
-to a new package! It is called `DelayEmbeddings`!
-Methods that estimate optimal delay/dimension are also in this
-package!\n
+A new package has joined DynamicalSystems: RecurrenceAnalysis !
+
+This new package offers tools to compute and analyze
+recurrences in your timeseries!
+
+Check out the documentation for more!\n
 """; color = :light_magenta)
 touch(joinpath(@__DIR__, update_name))
 end
