c012e5da2ffe449bae6e044b6b22591888c902b4
George Datseris <datseris.george@gmail.com>
2018-12-23 18:29:07 +0100

    add sliders for N, Ttr, density

--- src/interaction/orbitdiagram.jl
+++ src/interaction/orbitdiagram.jl
@@ -1,21 +1,29 @@
-using DynamicalSystems, Makie
-using DynamicalSystems, BenchmarkTools
-using DynamicalSystemsBase: DDS, MDI
+using DynamicalSystemsBase, Makie
 
 # add @inbounds when this is done.
 
+tslider = AbstractPlotting.textslider
 onlyleftclick(scplot) = (ispressed(scplot, Mouse.left) && !ispressed(scplot, Keyboard.space) &&
                    AbstractPlotting.is_mouseinside(scplot))
 
-function interactive_orbitdiagram(ds::DDS, i::Int, p_index, p_min, p_max;
-    density = 1000, u0 = get_state(ds), Ttr = 100, n = 100)
+function interactive_orbitdiagram(ds::DiscreteDynamicalSystem,
+    i::Int, p_index, p_min, p_max;
+    density = 1000, u0 = get_state(ds), Ttr = 100, n = 100
+    )
 
     # Initialization
     integ = integrator(ds)
-
+    scene = Scene(resolution = (1000, 600))
     pmin, pmax = p_min, p_max
     od = minimal_od(integ, i, p_index, pmin, pmax, density, n, Ttr, u0)
     od_node = Node(od)
+
+    ui_n, n = tslider(
+        round.(Int, range(10, stop=10000, length=1000)),"n", start=n)
+    ui_T, Ttr = tslider(
+        round.(Int, range(10, stop=10000, length=1000)),"Ttr", start=Ttr)
+    ui_d, density = tslider(
+        round.(Int, range(10, stop=10000, length=1000)),"density", start=density)
 
     scplot = scatter(od_node, markersize = 0.01)
 
@@ -23,20 +31,23 @@ function interactive_orbitdiagram(ds::DDS, i::Int, p_index, p_min, p_max;
         if onlyleftclick(scplot)
             pmin, xmin = mouseposition(scplot)
             # Get xmax, pmax with un-click
-            pmax = pmin + 0.2; xmax = xmin + 0.2
-            od_node[] = minimal_od(integ, i,  p_index, pmin, pmax,
-                                   density, n, Ttr, u0, xmin, xmax)
+            pmax = pmin + 0.2; xmax = xmin + 0.4
+            od_node[] = minimal_od(
+                integ, i,  p_index, pmin, pmax,
+                density[], n[], Ttr[], u0, xmin, xmax
+            )
 
             limits = FRect(pmin,xmin,pmax-pmin,xmax-xmin)
             # AbstractPlotting.update_limits!(scplot, limits)
         end
     end
-    display(scplot)
+    hbox(vbox(ui_n, ui_T, ui_d), scplot, parent=scene)
+    display(scene)
     return od_node
 end
 
 function minimal_od(integ, i, p_index, pmin, pmax,
-                    density, n, Ttr, u0, xmin = -Inf, xmax = Inf)
+                    density::Int, n::Int, Ttr::Int, u0, xmin = -Inf, xmax = Inf)
 
     pvalues = range(pmin, stop = pmax, length = density)
     od = Vector{Point2f0}()
@@ -52,7 +63,6 @@ function minimal_od(integ, i, p_index, pmin, pmax,
             end
         end
     end
-    integ.p[p_index] = pvalues[1]
     return od
 end
 
