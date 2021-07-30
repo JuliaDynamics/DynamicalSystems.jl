439e3be6d1e64a462336a2e936501e3c69b4c45f
Datseris <datseris.george@gmail.com>
2021-07-30 23:50:24 +0200

    add update message

--- src/DynamicalSystems.jl
+++ src/DynamicalSystems.jl
@@ -15,25 +15,37 @@ using Reexport
 @reexport using ChaosTools
 @reexport using RecurrenceAnalysis
 
-display_update = false
-update_name = "update_v1.4.0"
+# Update messages:
+using Scratch
+display_update = true
+version_number = "2.0"
+update_name = "update_v$(version_number)"
 
 if display_update
-if !isfile(joinpath(@__DIR__, update_name))
-printstyled(stdout,
-"""
-\nUpdate message: DynamicalSystems v1.3
-
-A method that estimates the predictability properties of a
-dynamical system has been implemented, following the work of:
-
-Wernecke, H., Sándor, B. & Gros, C.
-*How to test for partially predictable chaos*.
-
-See the function `predictability`.\n
-"""; color = :light_magenta)
-touch(joinpath(@__DIR__, update_name))
+    # Get scratch space for this package
+    versions_dir = @get_scratch!("versions")
+    if !isfile(joinpath(versions_dir, update_name))
+        printstyled(
+            stdout,
+            """
+            \nUpdate message: DynamicalSystems v$(version_number)
+            Welcome to this new (and breaking) release of DynamicalSystems.jl! 
+            All features, old and new, can be found in the online docs.
+            The *breaking* changes in this release are:
+            1. Keyword propagation to DifferentialEquations.jl does not happen anymore.
+               E.g.: `trajectory(ds, 100; reltol = 1e-9, alg = Tsit5())` is invalid.
+               Now keyword propagation to DiffEq happens always via the keyword
+               `diffeq`, whose value is a `NamedTuple/Dict` (the keyword-value pairs).
+               E.g.: `trajectory(ds, 100; diffeq = (reltol = 1e-9, alg = Tsit5())`.
+               Notice that this change affects a large list of fuctions of DynamicalSystems.jl!
+            2. If `A` is a `Dataset` then `A[range_of_integers]` now returns a `Dataset`.
+               Before it used to return `Vector{SVector}`.
+            """;
+            color = :light_magenta,
+        )
+        touch(joinpath(versions_dir, update_name))
+    end
 end
-end
+
 
 end # module
