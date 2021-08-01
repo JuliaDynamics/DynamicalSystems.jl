581283101f1b05757df64723547eb419938b67ee
Datseris <datseris.george@gmail.com>
2021-08-02 00:17:59 +0200

    uncom,ment

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 using Pkg
-# Pkg.activate(@__DIR__)
-# CI && Pkg.instantiate()
+Pkg.activate(@__DIR__)
+CI && Pkg.instantiate()
 
 using DynamicalSystems
 using Entropies, RecurrenceAnalysis, DelayEmbeddings, ChaosTools, DynamicalSystemsBase
@@ -12,9 +12,9 @@ import Downloads
 
 # %%
 # download the themes
-# for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
-#     Downloads.download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
-# end
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    Downloads.download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
+end
 # create the themes
 for w in ("light", "dark")
     header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
