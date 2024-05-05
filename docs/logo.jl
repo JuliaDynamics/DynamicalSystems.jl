299fdea72ef193d4627c9b95c7978eb765f05058
Datseris <datseris.george@gmail.com>
2024-05-05 15:14:14 +0100

    final logo (video pending)

--- docs/logo.jl
+++ docs/logo.jl
@@ -2,6 +2,8 @@ using DynamicalSystems
 using OrdinaryDiffEq
 using CairoMakie
 using DataStructures: CircularBuffer
+desktop() = joinpath(homedir(), "Desktop")
+desktop(args...) = joinpath(desktop(), args...)
 
 # double pendulum dynamical system
 @inbounds function doublependulum_rule(u, p, t)
@@ -55,9 +57,15 @@ traj = Observable(traj)
 
 # %% Initialize figure
 bgcolor = :transparent
+# bgcolor = :white
 fig = Figure(size = (800, 800), backgroundcolor = bgcolor)
-ax = Axis(fig[1,1]; backgroundcolor = bgcolor)
-ax.limits = ((-L1-L2-0.1, L1 + L2+0.1), (-L1-L2-0.1, L1 + L2 + 0.1))
+ax = Axis(fig[1,1]; backgroundcolor = bgcolor, aspect = DataAspect() )
+# this is maximum possible limits:
+# ax.limits = ((-L1-L2-0.1, L1 + L2+0.1), (-L1-L2-0.1, L1 + L2 + 0.1))
+# this is reduced size in height:
+ax.limits = ((-L1-L2-0.1, L1 + L2+0.1), (-L1-L2-0.1, (L1 + L2 + 0.1)/2))
+ylims!(-0.8660254037844386/2 - 2.1, -0.8660254037844386/2 + 2.1) # center y of axis for equal aspect ratio
+# ax.aspect = 1
 hidedecorations!(ax)
 hidespines!(ax)
 
@@ -73,13 +81,20 @@ lighter_red    = RGBf(0.835, 0.388, 0.361)
 lighter_blue   = RGBf(0.4, 0.51, 0.878)
 lighter_purple = RGBf(0.667, 0.475, 0.757)
 
+lighter_green  = julia_green
+lighter_red    = julia_red
+lighter_blue   = julia_blue
+lighter_purple = julia_purple
+
+# color of the trajectory (fading out)
 c = julia_blue
-tailcol = [RGBAf(c.r, c.g, c.b, (i/tail)^(1.2)) for i in 1:tail]
-# trajwidth = [4*(i/tail) for i in 1:tail]
+tailcoltransp = [RGBAf(c.r, c.g, c.b, (i/tail)^(1.2)) for i in 1:tail]
+tailcol = [RGBf(c.r, c.g, c.b) for i in 1:tail]
+trajline = lines!(ax, traj; color = tailcoltransp, linewidth = 4)
 
-trajline = lines!(ax, traj; color = tailcol, linewidth = 4)
 
-lines!(ax, balls; linewidth = 8, color = :black)
+# rods that connect the pendulum
+lines!(ax, balls; linewidth = 12, color = :black)
 
 scatter!(ax, balls; marker = :circle, strokewidth = 10,
     strokecolor = [julia_green, julia_red, julia_purple],
@@ -102,21 +117,12 @@ function animstep!(dp, t)
     end
 end
 
-
 fig
 
-# %% initial condition that leads to nice Julia logo, fantastic!
-u0 = [π/2, +0.1, 3π/4, -3]
-reinit!(dp, u0)
-
-animstep!(dp, 6.57)
-
-fig
 
 # %% find initial condition that leads to nice Julia logo:
-# u0 = [π/2 + 0.1, +0.03, 0.3, -3.78]
+using DynamicalSystems.StateSpaceSets.Distances
 u0 = [π/2 + 0.1, +2.03, 0.3, +5.412]
-# u0 = [π/2 + 0.1, +2.03, 0.3, +5.42]
 reinit!(dp, u0)
 
 
@@ -131,9 +137,6 @@ end
 
 d = distance_from_optimal(current_state(dp))
 
-
-# scatter!(ax, logocoords, markersize = 30)
-
 animstep!(dp, 6.56)
 
 while d > 0.01
@@ -147,19 +150,37 @@ end
 d
 tf = current_time(dp)
 
-fig
+# reported final time of evolution from given initial condition:
+# 168.38499999991936
+
+
+# okay, save high quality version:
+ax.backgroundcolor = :transparent
+CairoMakie.save(desktop("juliadynamics_logo.png"), fig; px_per_unit = 4)
+# and one without tail
+trajline.visible = false
+save(desktop("juliadynamics_logo_no_tail.png"), fig; px_per_unit = 4)
+trajline.visible = true
+# and one more with white background
+ax.backgroundcolor = :white
+CairoMakie.save(desktop("juliadynamics_logo_white.png"), fig; px_per_unit = 4)
+ax.backgroundcolor = :transparent
 
 # save(desktop("logo_transparent.png"), fig; px_per_unit = 4)
 # trajline.visible = false
 # save(desktop("logo_no_tail.png"), fig; px_per_unit = 4)
 # trajline.visible = true
 
-# %% animate from some t start to tf
-u0 = [π/2 + 0.1, +2.03, 0.3, +5.412]
+fig
+
+# %% perform video animation animate from some t start to tf
+ax.backgroundcolor = :transparent
 reinit!(dp, u0)
+resize!(fig, 800, 800)
 
 span = tail*dt
 ts = tf - span
+# ts += 20dt # for whatever reason we have to do this correction
 animstep!(dp, ts) # initial state
 
 # fig.backgroundcolor = :
@@ -167,41 +188,9 @@ dtrecord = dt*5
 frames = range(0, span; length = Int(span ÷ dtrecord))
 @show length(frames)
 
-record(fig, desktop("logo.mp4"), frames; framerate = 30) do i # i = frame number
+record(fig, desktop("juliadynamics_logo_anim.mp4"), frames; framerate = 30) do i # i = frame number
     animstep!(dp, dtrecord)
     fig
-end # for each step of this loop, a frame is recorded
-
-
-
-# %% step, animate
-
-animstep!(dp)
-
-fig
-
-
-# %% Search for a good initial condition
-using DynamicalSystems.StateSpaceSets.Distances
-function good_ic(u)
-    X, t = trajectory(dp, 1000.0, u; Δt = 0.25)
-    X = map(u -> xycoords(u)[3:4], X) # position of last ball
-    N = length(X)
-    D = fill(Inf, N, N)
-    for i in 1:N
-        for j in (i+1):N
-            D[i, j] = Euclidean()(X[i], X[j])
-        end
-    end
-
-    dmin, i = findmin(D)
-    t1, t2 = t[i[1]], t[i[2]]
-
-    # D = pairwise(Euclidean(), vec(X))
 end
 
-reinit!(dp, u)
-animstep!(dp, 304.5)
-fig
-animstep!(dp, 690.75 - 304.5)
 fig
