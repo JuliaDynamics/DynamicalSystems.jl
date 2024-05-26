2912cf302f2b6b341cc75d986003bd409201ddbb
Jonathan Fischer <jfisch27@jhu.edu>
2024-05-26 09:55:23 -0400

    Fixed Makie error in interactive_orbitdiagram (#239)
    * tests
    
    * fixed orbit error, added testset
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update Project.toml
    
    * remove commented code
    
    * increase version in Project.toml
    
    ---------
    
    Co-authored-by: George Datseris <datseris.george@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,17 @@
-using DynamicalSystems
+using DynamicalSystems, Test, Makie
+
+@testset "Interactive Orbit Diagram" begin
+    ps = Dict(
+    1 => 1:0.1:30,
+    2 => 10:0.1:50,
+    3 => 1:0.01:10.0)
+    pnames = Dict(1 => "σ", 2 => "ρ", 3 => "β")
+
+    lims = ((-30, 30), (-30, 30), (0, 100))
+
+    ds = PredefinedDynamicalSystems.lorenz()
+
+    fig, _ = interactive_orbitdiagram(ds, 1, 1.0, 3.0)
+    @test fig isa Makie.Figure
+end
+
